0d02725852048b1ad9e53033046a37340ad21387
Rohan Kadam <kadamrohan76@gmail.com>
2020-08-27 13:41:47 +0530

    sort by id -1 in mongo

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -1,14 +1,17 @@
 import React from "react";
 import axios from 'axios';
 import './HomePage.css';
+
 class HomePage extends React.Component{
 constructor(props){
     super(props);
 this.state={
-    clientData:"Init"
+    clientData:[],
+    img:""
 }
 this.handleApi=this.handleApi.bind(this);
 this.handleUpload=this.handleUpload.bind(this);
+this.uploadToServer=this.uploadToServer.bind(this);
 
 }
 
@@ -20,22 +23,65 @@ axios.get('/api').then(response=>{
     })
 })
 }
+uploadToServer(file){
+console.log("File");
+// console.log(file);
+const fd=new FormData();
+ fd.append("fileName",file);
+axios.post('/api',fd).then(response=>{
+    console.log(response);
+    this.setState({
+        
+        img:response.data
+    })
 
+//     const reader=new FileReader();
+// reader.onload=(e)=> {
+//         console.log(e.target.result);
+//         this.setState({
+//             img:e.target.result
+//         })
+//     };
+
+
+// reader.readAsDataURL(response.data);
+});
+
+}
 handleUpload(event){
-console.log(event.target.value);
+    event.preventDefault();
+console.log(event.target.img.files[0]);
+// const reader=new FileReader();
+// reader.onload=(e)=> {
+//         console.log(e.target.result);
+//         this.setState({
+//             img:e.target.result
+//         })
+//     };
+
+
+// reader.readAsDataURL(event.target.img.files[0]);
+this.uploadToServer(event.target.img.files[0]);
 }
 
 render(){
     return(
-        <div class="home">
+        <div className="home">
 <h1>You Frame</h1>
-<input type="text" id="inputBox"/>
         <button onClick={this.handleApi}>GET</button>
-        {this.state.clientData}
-        <form>
-            <input onChange={this.handleUpload} type="file"/>
+        <ol>
+            {this.state.clientData.map((data,index)=>{
+                return <li key={index}><img  height="50" width="50"  src={data.img.data}/></li>
+            })}
+        </ol>
+        {/* <img  height="50" width="50"  src={this.state.clientData[0]}/> */}
+        {/* {this.state.clientData} */}
+        <form onSubmit={this.handleUpload} method="post" encType="multipart/form-data">
+            <input type="file" id="img" name="img" accept="image/*"/>
+            <button type="submit" >Submit</button>
         </form>
-
+{/* <img height="50" width="50" src={this.state.img}/> */}
+{this.state.img}
         </div>
     )
 }
--- index.js
+++ index.js
@@ -1,15 +1,117 @@
+if(process.env.NODE_ENV!=='production'){
+    require('dotenv').config();
+
+}
 const express=require('express');
 const bodyParser=require('body-parser');
 const mongoose=require('mongoose');
-
-
+var multiparty=require('multiparty');
+var fs=require('fs');
 const app=express();
 app.use(bodyParser.json());
+var images=[];
+var imageLinks=[];
 
+const Schema=mongoose.Schema;
+
+var imageSchema=new Schema({
+    name:String,
+    size:Number,
+    img:{
+        data:String,
+        contentType:String
+    }
+});
+
+var imageModel=mongoose.model('imageModel',imageSchema);
+
+
+mongoose.connect(process.env.MONGODB,{useUnifiedTopology: true,useNewUrlParser: true},(err)=>{
+    if(err){
+
+    }else{
+console.log("Connected to database");
 app.get("/api",(req,res)=>{
     console.log("Logging on server");
-    res.send("Api called");
+    imageModel.find({},null,{sort:{_id:-1}},(err,docs)=>{
+if(err){
+
+}else{
+  //  console.log(docs);
+    res.send(docs);
+}
+    })
+
+//     for(let file of  images){
+// console.log(file.path);
+// fs.readFile(filepath,{encoding:'base64'},(err,data)=>{
+//     var img=document.createElement("img");
+//     img.src="data:image/png;base64, "+data;
+
+//     imagehtml.push(img);
+
+// });
+
+
+//     }
+
+    // res.send(imageLinks);
 });
+
+app.post("/api",(req,res)=>{
+    var form=new multiparty.Form();
+    form.parse(req,(err,fields,files)=>{
+        console.log("Image Received");
+        console.log(files);
+        console.log(fields);
+        console.log(req.headers)
+ images.push(files.fileName[0]);
+console.log(images.length);
+// const reader=new FileReader();
+fs.readFile(files.fileName[0].path,{encoding:'base64'},(err,data)=>{
+// console.log(data.toString('BASE64'));
+    // res.set({'Content-type':'image/jpg'});
+//     var img=document.createElement("img");
+//     img.src="data:image/png;base64, "+data;
+console.log(typeof(data));
+imageLinks.push("data:image/png;base64, "+data);
+let obj={
+    name:files.fileName[0].originalFilename,
+    size:files.fileName[0].size,
+    img:{
+        data:"data:image/png;base64, "+data,
+        contentType:"image/png"
+    }
+}
+var doc=new imageModel(obj);
+doc.save((err)=>{
+    if(err){
+
+    }else{
+        res.send(files.fileName[0].originalFilename+" Uploaded");
+    }
+})
+
+
+
+});
+// var imageAsBase64=fs.readFileSync(files.fileName[0].path,'base64');
+// console.log(imageAsBase64);
+// reader.onload=(e)=> {
+// res.send(e.target.result);
+// }
+// reader.readAsDataURL(files.fileName[0]);
+    })
+    
+})
+
+
+
+
+
+    }
+});
+
 
 const port=process.env.PORT || 5000;
 
